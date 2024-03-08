ee5b08c880f8227d4ffb205a24a09cf52cb2d9c2
Joseph Palmer <75081147+BlockBuilderJoe@users.noreply.github.com>
2024-03-08 17:59:28 +0000

    Added error handling for 0 and strings

--- calc.js
+++ calc.js
@@ -1,31 +1,71 @@
+
 //calculates VAT.
-function vatCalculator(inputNumber) {
-    var vat = inputNumber * 0.20
-    document.getElementById('vat').innerHTML = "VAT = " + vat.toLocaleString('en-GB', { style: 'currency', currency: 'GBP' });
-    document.getElementById('vat').style.visibility = 'visible';
+function vatCalculator(inputNumber, vat_output) {
+    let vat = inputNumber * 0.20
+    vat_output.innerHTML = "VAT = " + vat.toLocaleString('en-GB', { style: 'currency', currency: 'GBP' });
+    vat_output.style.color = ''; //resets the color after an error.
+    vat_output.style.visibility = 'visible';
     return vat;
 }
 
 //calculates Corportation Tax.
-function corpTaxCalculator(inputNumber, vat) {
-    var taxableIncome = inputNumber - vat;
-    var corpTax = taxableIncome * 0.19;
-    document.getElementById('corpTax').innerHTML = "Corporation tax = " + corpTax.toLocaleString('en-GB', { style: 'currency', currency: 'GBP' });
-    document.getElementById('corpTax').style.visibility = 'visible';
+function corpTaxCalculator(inputNumber, vat, corpTax_output) {
+    let taxableIncome = inputNumber - vat;
+    let corpTax = taxableIncome * 0.19;
+    corpTax_output.innerHTML = "Corporation tax = " + corpTax.toLocaleString('en-GB', { style: 'currency', currency: 'GBP' });
+    corpTax_output.style.color = ''; //resets the color after an error.
+    corpTax_output.style.visibility = 'visible';
     return corpTax;
 }
 
 //calculates total tax.
-function total(vat, corpTax) {
-    var totalTax = vat + corpTax;
-    document.getElementById('totalTax').innerHTML = "Total = " + totalTax.toLocaleString('en-GB', { style: 'currency', currency: 'GBP' });
-    document.getElementById('totalTax').style.visibility = 'visible';
+function total(vat, corpTax, totalTax_output) {
+    let totalTax = vat + corpTax;
+    totalTax_output.innerHTML = "Total = " + totalTax.toLocaleString('en-GB', { style: 'currency', currency: 'GBP' });
+    totalTax_output.style.visibility = 'visible';
+}
+
+//clears the output fields.
+function clear(corpTax_output, vat_output, totalTax_output) {
+    corpTax_output.style.visibility = 'hidden';
+    vat_output.style.visibility = 'hidden';
+    totalTax_output.style.visibility = 'hidden';
+}
+
+//changes vat and corpTax to error message and hides total tax.
+function error(corpTax_output, vat_output, totalTax_output) {
+    vat_output.innerHTML = "Even HMRC can't tax words!";
+    vat_output.style.color = 'red';
+    vat_output.style.visibility = 'visible';
+    corpTax_output.innerHTML = "Please enter a number.";
+    corpTax_output.style.color = 'red';
+    corpTax_output.style.visibility = 'visible';
+    totalTax_output.style.visibility = 'hidden';
+}
+
+//gets the output elements.
+function getOutputElements() {
+    let corpTax_output = document.getElementById('corpTax');
+    let vat_output = document.getElementById('vat');
+    let totalTax_output = document.getElementById('totalTax');
+    return [corpTax_output, vat_output, totalTax_output];
 }
 
 //triggered by typing in a number.
 function main() {
-    var inputNumber = document.getElementById('inputNumber').value;
-    var vat = vatCalculator(inputNumber);
-    var corpTax = corpTaxCalculator(inputNumber, vat);
-    total(vat, corpTax);
+    // Gets the output elements.
+    let [corpTax_output, vat_output, totalTax_output] = getOutputElements();
+    //gets the input value.
+    let inputNumber = document.getElementById('inputNumber').value;
+    if (isNaN(inputNumber)) {
+        error(corpTax_output, vat_output, totalTax_output);
+    }
+    else if (Number(inputNumber) === 0) {
+        clear(corpTax_output, vat_output, totalTax_output);
+    }
+    else {
+        let vat = vatCalculator(inputNumber, vat_output);
+        let corpTax = corpTaxCalculator(inputNumber, vat, corpTax_output);
+        total(vat, corpTax, totalTax_output);
+    }
 }
